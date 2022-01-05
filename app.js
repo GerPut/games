df9f16f613c0e8f1559457a19e0da697e2d3b393
Gerhard <gerhard.putter@yahoo.com>
2022-01-05 21:01:33 +0200

    Function to toggle themes.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const checkbox = document.getElementById('checkbox');
-
+var anchors = document.querySelectorAll('a');
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark');
+    anchors.forEach(x => x.classList.toggle('red'));
 });
